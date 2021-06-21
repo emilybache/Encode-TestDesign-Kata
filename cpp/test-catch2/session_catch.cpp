e321aa171a8d941b2ddc51c23dc182aec6cfd7ea
emily <emily@bacheconsulting.com>
2021-06-21 13:20:25 +0200

    solution

--- cpp/test-catch2/session_catch.cpp
+++ cpp/test-catch2/session_catch.cpp
@@ -1,41 +1,34 @@
 #include <SessionModificationCmd.h>
+#include <iostream>
 #include "ApprovalTests.hpp"
 #include "catch2/catch.hpp"
 #include "HexStringEncoder.h"
+
+using namespace std;
 
 
 TEST_CASE ("SessionModificationCmd") {
     SessionModificationCmd command(1, 1);
     ByteBuffer data;
-    command.setPqvl(1);
-    std::string hexStr;
     HexStringEncoder hex;
+    command.setPqvl(1);
 
     SECTION("MultiplesOfHours") {
         command.setXyzTimer(MultiplesOfHours, 23);
-
         command.encode(data);
-
-        hexStr = hex.encode(data);
-        REQUIRE(hexStr == "03010101083791");
+        REQUIRE(hex.encode(data) == "03010101083791");
     }
 
     SECTION("MultiplesOfMinutes") {
         command.setXyzTimer(MultipliesOfMinutes, 32); // outside range(31), expect 31
-
         command.encode(data);
-
-        hexStr = hex.encode(data);
-        REQUIRE(hexStr == "03010101085f91");
+        REQUIRE(hex.encode(data) == "03010101085f91");
     }
 
-    SECTION("TimerDeactivated") {
+    SECTION("TimerDeactivated"){
         command.setXyzTimer(TimerDeactivated, 2); // deactivated, expect value 0
-
         command.encode(data);
-
-        hexStr = hex.encode(data);
-        REQUIRE(hexStr == "03010101080091");
+        REQUIRE(hex.encode(data) == "03010101080091");
     }
 
 }
--- cpp/test-gtest/session_gtest.cpp
+++ cpp/test-gtest/session_gtest.cpp
@@ -9,15 +9,13 @@ using namespace std;
 
 class MessageTest : public ::testing::Test {
 
-    virtual void SetUp()
-    {
+    virtual void SetUp() {
         command = new SessionModificationCmd(1, 1);
         data = new ByteBuffer();
         hex = new HexStringEncoder();
     }
 
-    virtual void TearDown()
-    {
+    virtual void TearDown() {
         delete command;
         delete data;
         delete hex;
@@ -29,25 +27,7 @@ public:
     HexStringEncoder* hex;
 };
 
-TEST_F(MessageTest, TimerDeactivated) {
-    command->setXyzTimer(TimerDeactivated, 2); // deactivated, expect value 0
-    command->setPqvl(1);
-
-    command->encode(*data);
-
-    ASSERT_EQ(hex->encode(*data), "03010101080091");
-}
-
-TEST_F(MessageTest, MultiplesOfMinutes) {
-    command->setXyzTimer(MultipliesOfMinutes, 32); // outside range(31), expect 31
-    command->setPqvl(1);
-
-    command->encode(*data);
-
-    ASSERT_EQ(hex->encode(*data), "03010101085f91");
-}
-
-TEST_F(MessageTest, MultiplesOfHours) {
+TEST_F(MessageTest, SessionModificationCmd_MultiplesOfHours) {
     command->setXyzTimer(MultiplesOfHours, 23);
     command->setPqvl(1);
 
@@ -56,3 +36,20 @@ TEST_F(MessageTest, MultiplesOfHours) {
     ASSERT_EQ(hex->encode(*data), "03010101083791");
 }
 
+TEST_F(MessageTest, SessionModificationCmd_MultiplesOfMinutes) {
+    command->setPqvl(1);
+    command->setXyzTimer(MultipliesOfMinutes, 32); // outside range(31), expect 31
+
+    command->encode(*data);
+
+    ASSERT_EQ(hex->encode(*data), "03010101085f91");
+}
+
+TEST_F(MessageTest, SessionModificationCmd_TimerDeactivated) {
+    command->setPqvl(1);
+    command->setXyzTimer(TimerDeactivated, 2); // deactivated, expect value 0
+
+    command->encode(*data);
+
+    ASSERT_EQ(hex->encode(*data), "03010101080091");
+}
