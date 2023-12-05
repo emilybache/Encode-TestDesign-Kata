ab88059a3759df664fbbf5e4de4e5f35a4e5acec
Jon Reid <jon@qualitycoding.org>
2023-12-05 14:36:29 -0800

    Reverse expected/actual args for NUnit

--- csharp/EncodeTests/EncodeNUnitTest.cs
+++ csharp/EncodeTests/EncodeNUnitTest.cs
@@ -17,19 +17,19 @@ class EncodeNUnitTest
         var hex = new HexStringEncoder();
         var hexStr = hex.Encode(data);
         Console.Write("Hex: " + hexStr + "\n");
-        Assert.AreEqual(hexStr, "03010101083791");
+        Assert.AreEqual("03010101083791", hexStr);
 
         command.SetXyzTimer(XyzTimerUnit.MultipliesOfMinutes, 32); // outside range(31), expect 31
         command.Encode(data);
         hexStr = hex.Encode(data);
         Console.Write("Hex: " + hexStr + "\n");
-        Assert.AreEqual(hexStr, "03010101085f91");
+        Assert.AreEqual( "03010101085f91", hexStr);
 
         command.SetXyzTimer(XyzTimerUnit.TimerDeactivated, 2); // deactivated, expect value 0
         command.Encode(data);
         hexStr = hex.Encode(data);
         Console.Write("Hex: " + hexStr + "\n");
-        Assert.AreEqual(hexStr, "03010101080091");
+        Assert.AreEqual( "03010101080091", hexStr);
     }
 
     public class HexStringEncoder
